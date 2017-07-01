860a194122e23995315a62949c73eff870aa0377
MirandaMcCoy <mm0778076@otc.com>
2017-06-30 22:44:12 -0500

    Jumpy Frog - Unity Project and Game

---
+++ Assets/Scripts/LoadGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour {
+
+    private void Start()
+    {
+        Screen.SetResolution(960, 600, false);
+    }
+
+    // When user presses enter key, game scene is loaded
+    void Update () {
+		if (Input.GetKeyDown("return"))
+        {
+            SceneManager.LoadScene("Scene_GamePlay");
+        }
+	}
+}
